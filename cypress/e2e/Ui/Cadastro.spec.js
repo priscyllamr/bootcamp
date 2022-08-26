bb1edd05bae5f773ca3a2d341334946ec976c04f
priscyllamr <priscylla.reis@viavarejo.com.br>
2022-08-26 20:19:09 -0300

    exercicio 2

--- cypress/e2e/Ui/Cadastro.spec.js
+++ cypress/e2e/Ui/Cadastro.spec.js
@@ -12,7 +12,7 @@ describe('Funcionalidade: Cadastro', () => {
     })
     it.only('Deve criar perfil', () => {
         cy.cadastro('priscylla teste', '123456', faker.internet.email())
-        cy.criar_perfil('Bootcamp', 'https://conexaoqa.herokuapp.com/', 'Belo Horizonte', 'Cypress', '@priscyllamreis', 'eu sou qa!!')
+        cy.criar_perfil('Bootcamp', 'https://conexaoqa.herokuapp.com/', 'Belo Horizonte', 'Cypress', '@priscyllamreis', 'Eu sou um qa!!')
         cy.get('[data-test="alert"]').should('contain', 'Perfil Criado')
     })
 
